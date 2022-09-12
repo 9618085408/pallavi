aa2969ee0b463250fa66c70623a17971bc22d73e
Pallavi9381 <113325235+Pallavi9381@users.noreply.github.com>
2022-09-12 11:57:28 +0530

    Add files via upload

---
+++ user is eligible for vote or not 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter the age of the person:";
+	cin>>age;
+	if(age>=18)
+	{
+	cout<<"you are eligible for voting.";
+	}
+	else
+	{
+	cout<<"you are not elidible for voting.";
+	}
+	return 0;
+	}
+	
+
