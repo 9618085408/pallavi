72ea7c4a0d1336fd6e62a7b2cdf817008bf8d6d3
Pallavi9381 <113325235+Pallavi9381@users.noreply.github.com>
2022-09-13 11:03:55 +0530

    Add files via upload

---
+++ Biggest two numbers.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class biggest
+{
+	int x,y;
+	public:
+		int input();
+		int display();
+	};
+	int biggest::input()
+	{
+		cout<<"Enter the first number:";
+		cin>>x;
+		cout<<"Enter the second number:";
+		cin>>y;
+		}
+		int biggest::display(){
+		
+		if(x>y){
+			cout<<"x is the biggest number."<<endl;
+			}
+			else{
+			cout<<"y is the biggest number.";
+			}
+			}
+			int main(){
+			biggest a;
+			a.input();
+			a.display();
+			return 0;
+			}
+			
+		
+	
+
