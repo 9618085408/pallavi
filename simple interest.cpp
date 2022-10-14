fae0b5eeb1dfed404d3fa7ce7bef6846a69a614a
Pallavi <113019386+9618085408@users.noreply.github.com>
2022-10-14 20:58:53 +0530

    Add files via upload

--- simple interest.cpp
+++ simple interest.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 int main()
 {
-	int p,r,t,i;
-	cout<<"\n\n calculate the simple interest:\n";
-	cout<<"\n\n calculate the simple interest:\n";
-	cout<<"input the principle:";
-	cin>>p;
-	cout<<"input the rate of interest:";
-	cin>>r;cout<<"input the time:";
-	cin>>t;
-	i=(p*r*t)/100;
-	cout<<"the simple interest for the amount"<<p<<"for"<<t<<"years@"<<r<<"%is:"<<i;
-	cout<<endl;
-	return 0;
-	}
-	
+
+  float p, t, r, interest;
+
+  cout << "Enter principal amount, time and rate:";
+  cin >> p >> t >> r;
+
+
+  interest = (p*t*r)/100;
+
+
+  cout << "Interest = " << interest << endl;
+
+  return 0;
+}
